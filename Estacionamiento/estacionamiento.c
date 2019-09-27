a7333d49ae634ba48d2d2b754446eb9d636324d2
HecThor <52410304+OnettoHector@users.noreply.github.com>
2019-09-27 12:26:03 -0300

    Clase del 27

---
+++ Estacionamiento/estacionamiento.c
@@ -0,0 +1,94 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "estacionamiento.h"
+
+
+/*
+void hardcodearPersona (ePersona estasPersonas[], int cantidad)
+{
+    //ePersona es una estructura, estasPersonas (y luego persona) es un array de ePersonas hardcodeado
+
+
+    int ids[] = {1,2,3,4};
+    int dias [] = {33,22,25,11};
+    int meses [] = {3,4,12,11};
+    int anios [] = {1990,1980,2000,1995};
+    char nombres [][32] = {"maria","zeke","ger","yani"};
+    int i;
+    for (i=0; i<cantidad; i++)
+    {
+        estasPersonas[i].id=ids[i];
+        estasPersonas[i].fechaNac.anio=anios[i];
+        estasPersonas[i].fechaNac.mes=meses[i];
+        estasPersonas[i].fechaNac.dia=dias[i];
+        strcpy(estasPersonas[i].nombre,nombres[i]);
+    }
+
+}
+
+void mostrarPersona (ePersona semuestranPersonas [], int cantidad)
+{
+    int i;
+    for (i=0; i<cantidad; i++)
+    {
+        printf ("\n El nombre es: %s ", semuestranPersonas[i].nombre);
+        printf (" \t Dia: %02d ", semuestranPersonas[i].fechaNac.dia);
+        printf (" \t Mes: %02d ", semuestranPersonas[i].fechaNac.mes);
+        printf ("\t Anio: %04d \n\n", semuestranPersonas[i].fechaNac.anio);
+    }
+}
+
+void hardcodearVehiculos (eVehiculo estosVehiculos [], int cantidad)
+{
+    //eVehiculo es una estructura, estosVehiculos (y luego vehiculos) es un array de eVehiculos hardcodeado
+
+    int idDuenios[] = {1,2,2,4};
+    int ingresos [] = {10,9,8,11};
+    int salidas [] = {11,11,11,12};
+    char patentes [][32] = {"AAA111","BBB222","AA111BB","OJ222BB"};
+    //Para la fecha (todos hoy)
+    int dias [] = {26,26,26,26};
+    int meses [] = {9,9,9,9};
+    int anios [] = {2019,2019,2019,2019};
+    int i;
+    for (i=0; i<cantidad; i++)
+    {
+        estosVehiculos[i].horaIngreso =ingresos[i];
+        estosVehiculos[i].horaSalida =salidas[i];
+        estosVehiculos[i].idDuenio =idDuenios[i];
+        strcpy(estosVehiculos[i].patente,patentes[i]);
+
+        estosVehiculos[i].fechaIngreso.anio=anios[i];
+        estosVehiculos[i].fechaIngreso.mes=meses[i];
+        estosVehiculos[i].fechaIngreso.dia=dias[i];
+    }
+}
+
+void mostrarVehiculo (eVehiculo semuestranVehiculos [], ePersona estasPersonas[], int cantidad)
+{
+    int i;
+    for (i=0; i<cantidad; i++)
+    {
+        printf (" \n  El vehiculo: %d ", semuestranVehiculos[i].patente);
+
+        printf (" \Ingreso: %02d/%02d/%04d ",    semuestranVehiculos[i].fechaIngreso.dia,
+                                                    semuestranVehiculos[i].fechaIngreso.mes,
+                                                    semuestranVehiculos[i].fechaIngreso.anio);
+
+        printf ("\t Desde las: %02d ", semuestranVehiculos[i].horaIngreso);
+        printf (" Hasta a las: %02d \n\n", semuestranVehiculos[i].horaSalida);
+        int j;
+        for (j=0;j<CANT;j++)
+        {
+            if(semuestranVehiculos[i].idDuenio == estasPersonas[j].id)
+            {
+                printf ("Duenio: %s\n",estasPersonas[j].nombre);
+                break;
+            }
+        }
+
+        //printf ("\n Le duenie del vehiculo es: %s ", semuestranVehiculos[i].nombre);
+    }
+}
+
+*/
---
+++ Estacionamiento/estacionamiento.h
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/*void hardcodearPersona (ePersona[], int);
+void mostrarPersona (ePersona[], int);
+void hardcodearVehiculos (eVehiculo[], int);
+void mostrarVehiculo (eVehiculo[], ePersona[], int);
+*/
